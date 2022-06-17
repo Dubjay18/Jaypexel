d150785b13ccbe4a2bdfa73bab23b0bc861bebc6
Dubjay18 <jejeniyi7@gmail.com>
2022-06-17 13:47:08 +0100

    converted to a pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   images: {
     domains: ["images.pexels.com"],
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
